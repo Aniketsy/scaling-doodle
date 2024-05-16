b99f64ea6dca6d00fce94324820b2709de1b41b6
Aniketsy <148300120+Aniketsy@users.noreply.github.com>
2024-05-17 04:39:02 +0530

    Copilot second commit

--- member.js
+++ member.js
@@ -1,6 +1,12 @@
 function skillsMember() {
     return{
         restrict: `E`,
-        
+        templateuRL :'modules/skills/views/member.html',
+        controller :'SkillsMemberController',
+        controllerAs: 'vm',
+        bindToController: true,
+        scope:  {
+            member: 'm'
+        }
     }
 }
